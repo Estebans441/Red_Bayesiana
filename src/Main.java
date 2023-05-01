40dd710f70ca270c51dfc5b1c0fbc34e802753ad
Esteban Salazar <estebans441@gmail.com>
2023-05-01 00:01:05 -0500

    Prueba con ejemplo

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import entities.RedBayes;
 public class Main {
     public static void main(String[] args) {
         RedBayes red = new RedBayes("Tren", "ej.txt");
-        red.preds("Train");
+        System.out.println(red.Prob("Appointment=Attend | Maintenance=No, Rain=Light"));
+
     }
 }
