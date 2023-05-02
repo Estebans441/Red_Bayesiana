1fe085a452e894eeb3f4544eafbd47205790df39
Esteban Salazar <estebans441@gmail.com>
2023-05-01 20:12:35 -0500

    prueba con segundo ejemplo

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,7 @@ public class Main {
         RedBayes red = new RedBayes("Tren", "ej.txt");
         System.out.println(red.Prob("Appointment=Attend | Maintenance=No, Rain=Light"));
 
+        RedBayes red2 = new RedBayes("Lluvia", "ej2.txt");
+        System.out.println(red2.Prob("HierbaHumeda=T | Rociador=F, Lluvia=T"));
     }
 }
